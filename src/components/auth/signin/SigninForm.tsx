2e0882b2e7c8113b7a1634a30c87f927cbea32f1
unknown <jithinbp007@gmail.com>
2024-05-23 23:30:05 +0530

    sign in with google option added

--- src/components/auth/signin/SigninForm.tsx
+++ src/components/auth/signin/SigninForm.tsx
@@ -4,6 +4,7 @@ import { z } from "zod";
 
 import { signinSchema } from "./SigninForm.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Link } from "react-router-dom";
 
 type SigninFormType = z.infer<typeof signinSchema>;
 
@@ -23,46 +24,82 @@ function SigninForm() {
   };
 
   return (
-    <div className="border-[0.5px] border-gray-400 rounded-md p-5 md:p-10 mt-8">
-      <h1 className="text-xl font-bold text-center">LOG IN</h1>
-      <span className="block mt-4 text-sm text-center md:text-base">
-        Enjoy a personalised shopping experience
-      </span>
-      <form
-        className="flex flex-col gap-4 mt-4"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <TextField
-          id="email"
-          type="text"
-          {...register("email")}
-          label="Email *"
-          variant="outlined"
-          error={!!errors.email}
-          helperText={errors.email?.message}
-        />
+    <div>
+      <div className="border-[0.5px] border-gray-400 rounded-md p-5 md:p-10 mt-8">
+        <h1 className="text-xl font-bold text-center">LOG IN</h1>
+        <span className="block mt-4 text-sm text-center md:text-base">
+          Enjoy a personalised shopping experience
+        </span>
+        <form
+          className="flex flex-col gap-4 mt-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <TextField
+            id="email"
+            type="text"
+            {...register("email")}
+            label="Email *"
+            variant="outlined"
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
 
-        <TextField
-          id="password"
-          type="password"
-          {...register("password")}
-          label="Password *"
-          variant="outlined"
-          error={!!errors.password}
-          helperText={errors.password?.message}
-        />
+          <TextField
+            id="password"
+            type="password"
+            {...register("password")}
+            label="Password *"
+            variant="outlined"
+            error={!!errors.password}
+            helperText={errors.password?.message}
+          />
+          <Button
+            sx={{
+              backgroundColor: "rgb(185 28 28)",
+              fontWeight: "700",
+            }}
+            type="submit"
+            variant="contained"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+      <div className="sticky w-full h-[1px] mt-10 bg-slate-300">
+        <span className="absolute block text-sm -translate-x-1/2 bg-white text-slate-500 -top-2 w-max left-1/2 right-1/2">
+          New to zporty?
+        </span>
+
+        <Link to="/auth/signup">
+          <Button
+            sx={{
+              width: "100%",
+              marginBlockStart: "2rem",
+              marginBlockEnd: "3rem",
+              textTransform: "none",
+              borderColor: "rgb(185 28 28)",
+              color: "rgb(127 29 29)",
+              fontWeight: "700",
+            }}
+            variant="outlined"
+          >
+            Sign up for free
+          </Button>
+        </Link>
         <Button
-          className="bg-red-700"
           sx={{
-            backgroundColor: "rgb(185 28 28)",
+            width: "100%",
+            marginBlockStart: "2rem",
+            marginBlockEnd: "3rem",
+            textTransform: "none",
             fontWeight: "700",
           }}
-          type="submit"
-          variant="contained"
+          variant="outlined"
         >
-          Login
+          <img src="/images/logos/google_logo.svg" width={20} height={20} />{" "}
+          <span className="block ml-auto">Sign in with Google</span>
         </Button>
-      </form>
+      </div>
     </div>
   );
 }
