e58bdd6c8612d5fc8ff3d0f874acece0434223d7
unknown <jithinbp007@gmail.com>
2024-05-25 21:21:38 +0530

    updating the schema

--- src/components/auth/signin/SigninForm.schema.ts
+++ src/components/auth/signin/SigninForm.schema.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
 export const signinSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
+  email: z.string().email({ message: "Enter a valid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must contain at least 8 character(s)" }),
 });
--- src/components/auth/signup/SignupForm.schema.ts
+++ src/components/auth/signup/SignupForm.schema.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 
 export const SignupSchema = z.object({
-  firstname: z.string().min(1),
-  lastname: z.string().min(1),
-  email: z.string().email(),
-  password: z.string().min(8),
+  firstname: z.string().min(1, { message: "Required" }),
+  lastname: z.string().min(1, { message: "Required" }),
+  email: z.string().email({ message: "Enter a valid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must contain at least 8 character(s)" }),
 });
